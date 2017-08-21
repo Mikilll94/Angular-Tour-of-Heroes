26683333c0de77fcdf1fc5d58ff96e980ad9153d
Mikołaj Waśniewski <wasniewskimikolaj@gmail.com>
2017-08-21 16:27:18 +0200

    CRUD ready

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,31 +9,41 @@ import { Router } from "@angular/router";
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
+  title = 'Tour of Heroes';
+  heroes = [];
+  selectedHero: Hero;
   ngOnInit(): void {
     this.getHeroes();
   }
-
   constructor(private heroService: HeroService,
-    private router: Router
-  ) {
-
-  }
-
-  title = 'Tour of Heroes';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-  heroes = [];
-  selectedHero: Hero;
+    private router: Router) {}
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.create(name)
+      .then(hero => {
+        this.heroes.push(hero);
+        this.selectedHero = null;
+      });
+  }
+
   getHeroes(): void {
     this.heroService.getHeroes()
       .then(heroes => this.heroes = heroes);
+  }
+
+  delete(hero: Hero): void {
+    this.heroService
+        .delete(hero.id)
+        .then(() => {
+          this.heroes = this.heroes.filter(h => h !== hero);
+          if (this.selectedHero === hero) { this.selectedHero = null; }
+        });
   }
 
   gotoDetail(): void {
